312edd14a95be3d4969b1107aea5c83b69872a3c
Bruno Sanches <bruno_sanches15@hotmail.com>
2016-03-24 08:25:08 -0300

    Continuacao de Route (separacao do formulario de busca)

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,11 @@
 
 	var app = angular.module("githubViewer", ["ngRoute"]);
 
-	app.config(function($routeProvider) { //Inicia as configuracoes do Angular
+	app.config(function($routeProvider) { //Inicia as configuracoes do modulo
 
 		$routeProvider
 			.when("/main", { //Quando a URL do navegador for .../main, sera identificado e trocara o template e a controller responssaveis
-				templateUrl: "main.html";
+				templateUrl: "main.html",
 				controller: "MainController"
 			})
 			.otherwise({redirectTo: "/main"}); //Se a URL for desconhecida, redirecionar para /main
